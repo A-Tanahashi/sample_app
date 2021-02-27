c7e3e7fa672ab19b386a976d89928c12943663a3
Ubuntu <ubuntu@ip-172-31-46-191.ap-northeast-1.compute.internal>
2021-02-27 22:57:50 +0000

    Finish ch05

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,23 @@
 Rails.application.routes.draw do
+
   root 'static_pages#home'
   #root 'application#hello'
   
-  get 'static_pages/home'
+  #get 'static_pages/home'
   # => Static_paged#home
   
-  get 'static_pages/help'
+  #get 'static_pages/help'
   # => Static_paged#help
+  get '/help', to:'static_pages#help'
   
-  get 'static_pages/about'
+  #get 'static_pages/about'
   # => Static_paged#about
+  get '/about', to:'static_pages#about'
   
-  get 'static_pages/contact'
+  #get 'static_pages/contact'
   # => Static_paged#contact
+  get '/contact', to:'static_pages#contact'
+  
+  get '/signup', to:'users#new'
   
 end
